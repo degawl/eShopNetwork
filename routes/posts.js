70bfc0993c7a9339f39ba9ce6914146c51832314
degawl <devongawley@gmail.com>
2018-10-07 22:30:42 +0300

    Added base routes

---
+++ routes/posts.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.send('INDEX /posts');
+});
+
+router.get('/new', (req, res, next) => {
+    res.send('NEW /posts/new');
+});
+
+router.post('/', (req, res, next) => {
+    res.send('CREATE /posts/');
+});
+
+router.get('/:id', (req, res, next) => {
+    res.send('SHOW /posts/:id');
+});
+
+router.get('/:id/edit', (req, res, next) => {
+    res.send('EDIT /posts/:id/edit');
+});
+
+router.put('/:id', (req, res, next) => {
+    res.send('UPDATE /posts/:id');
+});
+
+router.delete('/:id', (req, res, next) => {
+    res.send('DELETE /posts/:id');
+});
+
+module.exports = router;
