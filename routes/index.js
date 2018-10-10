82b49224c921b7e91ccfea9053abfe593e1679c7
degawl <devongawley@gmail.com>
2018-10-10 18:26:37 +0300

    added user routes and password reset routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'eShop Network - Home' });
+});
+
+router.get('/register', (req, res, next) => {
+  res.send('GET /register')
+});
+
+router.post('/register', (req, res, next) => {
+  res.send('POST /register')
+});
+
+router.get('/login', (req, res, next) => {
+  res.send('GET /login')
+});
+
+router.post('/login', (req, res, next) => {
+  res.send('POST /login')
+});
+
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile')
+});
+
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('PUT /profile/:user_id  ')
+});
+
+router.get('/forgot', (req, res, next) => {
+  res.send('GET /forgot')
+});
+
+router.put('/forgot', (req, res, next) => {
+  res.send('PUT /forgot')
+});
+
+router.get('/reset/:token', (req, res, next) => {
+  res.send('GET /reset')
+});
+
+router.put('/reset/:token', (req, res, next) => {
+  res.send('PUT /reset')
 });
 
 module.exports = router;
