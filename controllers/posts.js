a6eabb097a6c657cff237206618c0a3cd2b36d5e
Isabel Palumäe <ispalu@ttu.ee>
2018-10-31 12:08:31 +0200

    erroriga show.ejs

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mbxGeocoding({accessToken: process.env.MAPBOX_TOKEN});
 const cloudinary = require('cloudinary');
 cloudinary.config({
 	cloud_name: 'drxxvydxk',
@@ -23,6 +25,13 @@ module.exports = {
 				public_id: image.public_id
 			});
 		}
+		let response = await geocodingClient
+  			.forwardGeocode({
+    		query: req.body.post.location,
+    		limit: 1
+  		})
+		  .send();
+		  req.body.post.coordinates = response.body.features[0].geometry.coordinates;
 		let post = await Post.create(req.body.post);
 		res.redirect(`/posts/${post.id}`);
 	},
