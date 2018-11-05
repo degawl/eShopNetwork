1a2728bb512b6006f37780e2b134127e95d08d0d
Reiko <reiko.magi@gmail.com>
2018-11-05 03:30:15 +0200

    Layouts v2

--- app.js
+++ app.js
@@ -57,6 +57,10 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next) {
   res.locals.title = 'e-Shop Network'
+  res.locals.success = req.session.success ||'';
+  delete req.session.success;
+  res.locals.error = req.session.error ||'';
+  delete req.session.error;
   next();
 });
 
@@ -72,12 +76,15 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //res.locals.message = err.message;
+  //res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  //res.status(err.status || 500);
+  //res.render('error');
+  console.log(err);
+  req.session.error = err.message;
+  res.redirect('back');
 });
 
 module.exports = app;
