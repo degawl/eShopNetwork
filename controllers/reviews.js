baf8ce957afbc769ae283466c48e04f64d8aed42
Isabel Palumäe <ispalu@ttu.ee>
2018-11-05 13:22:35 +0200

    Spontaneous break

---
+++ controllers/reviews.js
@@ -0,0 +1,14 @@
+const Post = require('../models/post');
+const Review = require('../models/review');
+
+module.exports = {
+	async reviewCreate(req, res, next) {
+		
+	},
+	async reviewUpdate(req, res, next) {
+		
+	},
+	async reviewDestroy(req, res, next) {
+		
+	}
+}
--- models/review.js
+++ models/review.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema({
-    body: String,    
+    body: String,   
+    rating: Number, 
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User'
