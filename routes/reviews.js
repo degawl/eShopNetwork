5078378552318efc4a640262d5fa8b0b5d24bf79
Isabel Palumäe <ispalu@ttu.ee>
2018-11-18 15:03:07 +0200

    Authorization works in reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true});
-const { asyncErrorHandler } = require('../middleware');
+const { asyncErrorHandler, isReviewAuthor } = require('../middleware');
 const{
     reviewCreate,
     reviewUpdate,
@@ -9,7 +9,7 @@ const{
 
 router.post('/', asyncErrorHandler(reviewCreate));
 
-router.put('/:review_id', asyncErrorHandler(reviewUpdate));
+router.put('/:review_id', isReviewAuthor, asyncErrorHandler(reviewUpdate));
 router.delete('/:review_id', (req, res, next) => {
     res.send('DELETE /posts/:id/reviews/:review_id');
 });
