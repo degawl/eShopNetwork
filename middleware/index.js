1bff68fe020a6f7198afa2653481187f5d4f2cff
degawl <devongawley@gmail.com>
2018-11-19 15:19:20 +0200

    Middleware refactor

--- middleware/index.js
+++ middleware/index.js
@@ -1,17 +1,64 @@
-const Review = require('../models/review');
+const Post = require('../models/post');
+const cloudinary = require('cloudinary');
+cloudinary.config({ 
+  cloud_name: 'drxxvydxk', 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 module.exports = {
-    asyncErrorHandler: (fn) =>
-        (req, res, next) => {
-            Promise.resolve(fn(req, res, next))
-                   .catch(next);
-    },
-isReviewAuthor: async(req, res, next)=>{
-    let review = await Review.findById(req.params.review_id);
-    if(review.author.equals(req.user._id)){
-        return next();
-    }
-    req.session.error = 'Bye bye';
-    return res.redirect('/');
-}
+	asyncMiddleware: fn =>
+	  (req, res, next) => {
+	    Promise.resolve(fn(req, res, next))
+	      .catch(next);
+		},
+	isLoggedIn: (req, res, next) => {
+	    if (req.isAuthenticated()) return next();
+	    if (req.xhr) {
+	    	return res.json({'loginError': true});
+	    }
+	    req.session.redirectTo = req.originalUrl;
+		  req.flash('error', 'You need to be logged in to do that!');
+	    res.redirect('/login');
+	},
+	sanitizeBody: (req, res, next) => {
+		if (req.body.post.description) {
+			req.body.post.description = req.sanitize(req.body.post.description);
+		}
+		next();
+	},
+	checkPostOwner: async (req, res, next) => {
+		try {
+				let post = await Post.findById(req.params.id);
+				if (!post.author._id.equals(req.user._id)) {
+					req.flash('error', 'You\'re not the owner of this post.');
+					return res.redirect('/');
+				}
+				req.post = post;
+				next();
+		} catch(err) {
+				return next(err);
+		}
+	},
+	findLocation: function(req, res, next) {  
+	  var limit = req.query.limit || 10;
+	  var maxDistance = req.query.distance || 8;
+	  maxDistance /= 6371;
+
+	  var coords = [];
+	  coords[0] = req.query.longitude;
+	  coords[1] = req.query.latitude;
+
+	  Post.find({
+	    coordinates: {
+	      $near: coords,
+	      $maxDistance: maxDistance
+	    }
+	  }).limit(limit).exec(function(err, posts) {
+	    if (err) {
+	      return res.json(500, err);
+	    }
+	    res.json(200, posts);
+	  });
+	}
 }
